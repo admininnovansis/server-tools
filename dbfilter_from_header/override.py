d996d5b7b3f03afd65f7eafeb81b43af9a95e4c4
admininnovansis <admin@innovansis.com>
2024-12-21 09:46:04 -0500

    [IMP] Decors header with start and end regex characters
    It is used to avoid conflicts start and end characters for regex in nginx header is setted

--- dbfilter_from_header/override.py
+++ dbfilter_from_header/override.py
@@ -12,12 +12,15 @@ from odoo.tools import config
 db_filter_org = http.db_filter
 
 
-def db_filter(dbs, httprequest=None):
-    dbs = db_filter_org(dbs, httprequest)
-    httprequest = httprequest or http.request.httprequest
+def db_filter(dbs, host=None):
+    dbs_orig = db_filter_org(dbs, host)
+    httprequest = http.request.httprequest
     db_filter_hdr = httprequest.environ.get("HTTP_X_ODOO_DBFILTER")
+    rex = f"^{db_filter_hdr}$"
     if db_filter_hdr:
-        dbs = [db for db in dbs if re.match(db_filter_hdr, db)]
+        dbs = [db for db in dbs if re.match(rex, db)]
+    else:
+        dbs = dbs_orig
     return dbs
 
 
